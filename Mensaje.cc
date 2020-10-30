5159a8ada97462d0ac67d0dc793809ad01731ea5
Ignacio-Viquez <joseignacioviquezrojas@gmail.com>
2020-10-30 01:08:04 -0600

    Creacion de las clases

---
+++ Mensaje.cc
@@ -0,0 +1,8 @@
+#include "Mensaje.h"
+
+
+Mensaje::Mensaje(){
+	printf("Hola Pera, te ves muy linda el dia de hoy...\n");
+}
+
+Mensaje::~Mensaje(){}
---
+++ main.cc
@@ -0,0 +1,12 @@
+#include "Mensaje.h"
+
+/*
+ * main del proyecto 
+*/
+int main(int argc, char const *argv[])
+{
+	/* code */
+	Mensaje * MensajeParaPera = new Mensaje();
+	delete(MensajeParaPera);
+	return 0;
+}
